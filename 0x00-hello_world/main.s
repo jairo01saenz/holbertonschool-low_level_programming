8d15dfc094a36102fb4c273eb7d13c6b0a6cd450
jairo sanchez <2773@holbertonschool.com>
2021-02-05 22:42:51 +0000

    is intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,21 +1,22 @@
-    .file   "main.c"
-    .text
-    .globl  main
-    .type   main, @function
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.globl	main
+	.type	main, @function
 main:
 .LFB0:
-    .cfi_startproc
-    pushq   %rbp
-    .cfi_def_cfa_offset 16
-    .cfi_offset 6, -16
-    movq    %rsp, %rbp
-    .cfi_def_cfa_register 6
-    movl    $0, %eax
-    popq    %rbp
-    .cfi_def_cfa 7, 8
-    ret
-    .cfi_endproc
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
 .LFE0:
-    .size   main, .-main
-    .ident  "GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
-    .section    .note.GNU-stack,"",@progbits
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
